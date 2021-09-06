6540333383d89e4198e6da37f438e98d1a97f2f4
Evelyn Anyebe <anyebeevelyn@gmail.com>
2021-09-06 02:34:55 +0100

    landing page integration

--- scrum-frontend/src/App.jsx
+++ scrum-frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import './App.module.css';
+import NavigationBar from './components/Navigation/NavigationBar';
+import Home from './pages/Home';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavigationBar />
+      <Switch>
+        <Route path="/home" component={Home} />
+      </Switch>
     </div>
   );
 }
